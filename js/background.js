bcf7bdf6198eef60d81c8e32c8bcfd9420b8d61d
tech234a <46801700+tech234a@users.noreply.github.com>
2019-01-20 17:14:59 -0500

    Implement simple options

--- js/background.js
+++ js/background.js
@@ -1,16 +1,62 @@
 // testing endpoint until an actual api with all the annotation data is available
 const annotationsEndpoint = "https://archive.omar.yt/api/v1/annotations/";
 
+//set default options, load user-set options
+let optarchived;
+let optmodern;
+let optpause;
+
+chrome.storage.sync.get(['optarchived'], function (result) {
+	if (result.optarchived === undefined) {
+		chrome.storage.sync.set({
+			optarchived: true
+		}, function () {
+			console.log('Option Saved')
+			optarchived = true
+		});
+	} else {
+		optarchived = result.optarchived;
+	};
+});
+chrome.storage.sync.get(['optmodern'], function (result) {
+	if (result.optmodern === undefined) {
+		chrome.storage.sync.set({
+			optmodern: true
+		}, function () {
+			console.log('Option Saved')
+			optmodern = true
+		});
+	} else {
+		optmodern = result.optmodern;
+	};
+});
+chrome.storage.sync.get(['optpause'], function (result) {
+	if (result.optpause === undefined) {
+		chrome.storage.sync.set({
+			optpause: false
+		}, function () {
+			console.log('Option Saved')
+			optpause = false
+		});
+	} else {
+		optpause = result.optpause;
+	};
+});
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 	if (changeInfo.status === "complete" && tab.url.startsWith("https://www.youtube.com/watch?")) {
 		const url = new URL(tab.url);
 		const videoId = url.searchParams.get("v");
 
-		chrome.tabs.sendMessage(tab.id, {type: "remove_renderer_annotations"});
+		chrome.tabs.sendMessage(tab.id, {
+			type: "remove_renderer_annotations"
+		});
 
 		if (videoId) {
 
-			chrome.tabs.sendMessage(tab.id, {type: "check_description_for_annotations"}, response => {
+			chrome.tabs.sendMessage(tab.id, {
+				type: "check_description_for_annotations"
+			}, response => {
 				console.log(response);
 				if (response.requestAnnotations) {
 					const requestUrl = annotationsEndpoint + videoId;
@@ -20,17 +66,22 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 					.then(response => response.text())
 					.then(text => {
 						if (text) {
-							chrome.tabs.sendMessage(tab.id, {type: "annotations_received", xml: text});
-						}
-						else {
-							chrome.tabs.sendMessage(tab.id, {type: "annotations_unavailable"});
+							chrome.tabs.sendMessage(tab.id, {
+								type: "annotations_received",
+								xml: text
+							});
+						} else {
+							chrome.tabs.sendMessage(tab.id, {
+								type: "annotations_unavailable"
+							});
 						}
 					}).catch(e => {
 						console.log("Annotation data is unavailable for this video");
-						chrome.tabs.sendMessage(tab.id, {type: "annotations_unavailable"});
+						chrome.tabs.sendMessage(tab.id, {
+							type: "annotations_unavailable"
+						});
 					});
-				}
-				else {
+				} else {
 					console.info("Annotations found in description..");
 				}
 			});
