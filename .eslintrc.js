cd2c50b989f7ded8ce87dd7fa4c1169754bd70ba
afrmtbl <afrmtbl@gmail.com>
2022-04-07 20:04:37 -0400

    Add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "webextensions": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+
+    },
+    "globals": {
+        "AnnotationParser": true,
+        "AnnotationRenderer": true
+    }
+}
