fb031f5b879d69f56f299b97a3951e6e1d066f58
afrmtbl <afrmtbl@gmail.com>
2022-09-12 22:20:10 -0400

    Add locales directory to build script

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 mkdir -p build
 
 echo "Building for Chrome..."
-zip build/chrome_build.zip icons/* popup/* manifest.json pages/* js/annotationlib/dist/* js/*.js
+zip -r build/chrome_build.zip icons/* _locales/* popup/* manifest.json pages/* js/annotationlib/dist/* js/*.js
 
 mv manifest.json manifest_chrome.json
 mv manifest_firefox.json manifest.json
 
 echo "Building for Firefox..."
-zip build/firefox_build.zip icons/* popup/* manifest.json pages/* js/annotationlib/dist/* js/*.js
+zip -r build/firefox_build.zip icons/* _locales/* popup/* manifest.json pages/* js/annotationlib/dist/* js/*.js
 
 mv manifest.json manifest_firefox.json
 mv manifest_chrome.json manifest.json
